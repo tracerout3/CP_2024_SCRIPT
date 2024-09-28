f412e6d94720022ca9313238c040e221ee16885d
tracerout3 <Tracerout3@proton.me>
2024-09-28 18:52:55 +0000

    Update script.sh
    added function to check sudoers

--- script.sh
+++ script.sh
@@ -143,6 +143,31 @@ fail2ban() {
 
 
 
+sudo() {
+	for x in `cat users`
+	do
+		read -p "Is $x considered an admin?[y/n]: " a
+		if [ $a = y ]
+		then
+			##Adds to the adm group
+			sudo usermod -a -G adm $x
+
+			##Adds to the sudo group
+			sudo usermod -a -G sudo $x
+		else
+			##Removes from the adm group
+			sudo deluser $x adm
+
+			##Removes from the sudo group
+			sudo deluser $x sudo
+		fi
+	done
+
+	pause
+}
+
+
+
 
 
 
@@ -156,6 +181,7 @@ show_menu() {
     echo "3) Change bad passwords ("all passwords will be CyB3rP@tr1oT2024")"
     echo "4) Config Firewall"
     echo "5) Secure SSH"
+    echo "6) Check sudoers"
     echo "q) Quit"
 }
 
@@ -183,6 +209,10 @@ while true; do
             fail2ban
             ;;
         
+
+        5)
+            sudo
+            ;;
         q)
             echo "Exiting..."
             break
